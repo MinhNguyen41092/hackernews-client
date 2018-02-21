f14d41be860e2ca589afedb54e4051f0216bbeaa
Nguyen Dinh Minh <minh.nguyendinh@asnet.com.vn>
2018-02-21 15:19:17 +0700

    write 3 more styled components

--- src/components/Styles.jsx
+++ src/components/Styles.jsx
@@ -7,15 +7,43 @@ export const Wrapper = styled.div`
   margin: 8px;
 
   box-shadow: 1px 1px 5px #888888;
-`
+`;
 
 export const HeaderWrapper = styled.div`
   display: flex;
   flex-direction: row;
   font-size: 10px;
-`
+`;
 
 export const RankContainer = styled.div`
   margin: 0px 5px;
   color: #828282;
-`
+`;
+
+export const Title = styled.a.attrs({
+  target: "_blank",
+  rel: "test"
+})`
+  color: #000;
+  text-decoration: none;
+  margin: 0px 2px;
+
+  &:visited {
+    color: #828282;
+  }
+`;
+
+export const SourceContainer = styled.span`
+  font-size: 10px;
+  margin-top: 1px;
+  color: #828282;
+`;
+
+export const SourceLink = styled.a.attrs({
+  target: "_blank",
+  rel: "test"
+})`
+  color: #828282;
+  text-decoration: none;
+  margin: 0px 1px;
+`;
