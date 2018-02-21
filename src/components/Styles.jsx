c2f9b3fab5db5560aa0a0858f558fe97f80b60dc
Nguyen Dinh Minh <minh.nguyendinh@asnet.com.vn>
2018-02-21 15:08:25 +0700

    write Wrapper component

---
+++ src/components/Styles.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  background-color: #f6f6ef;
+  padding: 8px;
+  margin: 8px;
+
+  box-shadow: 1px 1px 5px #888888;
+`
