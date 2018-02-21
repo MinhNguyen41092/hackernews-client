91288797108f2d1c9f585cd4cef315854da964f2
Nguyen Dinh Minh <minh.nguyendinh@asnet.com.vn>
2018-02-21 15:02:30 +0700

    write Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+export default function Header(props) {
+  let sourceUrl = utils.getSourceUrl(props.url);
+
+  return (
+    <HeaderWrapper>
+      <RankContainer>
+        {props.rank}
+      </RankContainer>
+
+      <Title href={props.url}>
+        {props.title}
+      </Title>
+
+      <SourceContainer>
+        (<SourceLink href={"http://" + sourceUrl}>{sourceUrl}</SourceLink>)
+      </SourceContainer>
+    </HeaderWrapper>
+  )
+}
+
+Header.propTypes = {
+  rank: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired
+}
