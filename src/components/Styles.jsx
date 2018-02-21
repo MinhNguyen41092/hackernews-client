0552e0ef0fb05dbd55a5270b98f10ce133176b20
Nguyen Dinh Minh <minh.nguyendinh@asnet.com.vn>
2018-02-21 15:09:35 +0700

    write HeaderWrapper component

--- src/components/Styles.jsx
+++ src/components/Styles.jsx
@@ -8,3 +8,9 @@ export const Wrapper = styled.div`
 
   box-shadow: 1px 1px 5px #888888;
 `
+
+export const HeaderWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  font-size: 10px;
+`
